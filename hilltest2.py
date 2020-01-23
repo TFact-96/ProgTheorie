ed4f98513ad70f46c6774fbd9da2ad87bf0dd143
itsrelative1 <arkin.mehmet14@hotmail.com>
2020-01-23 12:26:42 +0100

    changes

--- hilltest2.py
+++ hilltest2.py
@@ -53,171 +53,170 @@ class Grid_point:
 
 class Grid:
     def __init__(self, amino):
-        self.grid = {}
-        self.grid_chain = {}
-        self.hh_bonds = []
 
         self.amino = amino
         self.best_chain = {}
         self.diagonal_moves = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
         self.moves = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-        self.stability = 0
-
     # Create n x n grid
     def create_grid(self, n):
+        grid = {}
         for y in range(-n, n + 1):
             for x in range(-n, n + 1):
-                self.grid[f"{x, y}"] = Grid_point(False, [x, y])
+                grid[f"{x, y}"] = Grid_point(False, [x, y])
 
-    def clear_grid(self):
-        for key, value in self.grid.items():
-            self.grid[key].filled = False
-            self.grid[key].nodes = []
-            self.stability = 0
-            self.hh_bonds = []
+        return grid
 
-    def add_neighbours(self, node):
+    def clear_grid(self, grid):
+        for key, value in grid.items():
+            grid[key].filled = False
+            grid[key].nodes = []
+
+    def add_neighbours(self, node, grid, hh_bonds):
         x = node.x
         y = node.y
         n = node.n
 
         if node.type == "P":
-            return 0
+            return False
 
         if (
-            self.grid[f"{x + 1, y}"].filled
-            and (abs(self.grid[f"{x + 1, y}"].nodes[0].n - n) > 1)
-            and (self.grid[f"{x + 1, y}"].nodes[0].type == "H")
+            grid[f"{x + 1, y}"].filled
+            and (abs(grid[f"{x + 1, y}"].nodes[0].n - n) > 1)
+            and (grid[f"{x + 1, y}"].nodes[0].type == "H")
         ):
-            if [[x, x + 1], [y, y]] and [[x + 1, x], [y, y]] not in self.hh_bonds:
-                self.hh_bonds.append([[x, x + 1], [y, y]])
+            if [[x, x + 1], [y, y]] and [[x + 1, x], [y, y]] not in hh_bonds:
+                hh_bonds.append([[x, x + 1], [y, y]])
 
         if (
-            self.grid[f"{x, y + 1}"].filled
-            and (abs(self.grid[f"{x, y + 1}"].nodes[0].n - n) > 1)
-            and (self.grid[f"{x, y + 1}"].nodes[0].type == "H")
+            grid[f"{x, y + 1}"].filled
+            and (abs(grid[f"{x, y + 1}"].nodes[0].n - n) > 1)
+            and (grid[f"{x, y + 1}"].nodes[0].type == "H")
         ):
-            if [[x, x], [y, y + 1]] and [[x, x], [y + 1, y]] not in self.hh_bonds:
-                self.hh_bonds.append([[x, x], [y, y + 1]])
+            if [[x, x], [y, y + 1]] and [[x, x], [y + 1, y]] not in hh_bonds:
+                hh_bonds.append([[x, x], [y, y + 1]])
 
         if (
-            self.grid[f"{x - 1, y}"].filled
-            and (abs(self.grid[f"{x - 1, y}"].nodes[0].n - n) > 1)
-            and (self.grid[f"{x - 1, y}"].nodes[0].type == "H")
+            grid[f"{x - 1, y}"].filled
+            and (abs(grid[f"{x - 1, y}"].nodes[0].n - n) > 1)
+            and (grid[f"{x - 1, y}"].nodes[0].type == "H")
         ):
-            if [[x, x - 1], [y, y]] and [[x - 1, x], [y, y]] not in self.hh_bonds:
-                self.hh_bonds.append([[x, x - 1], [y, y]])
+            if [[x, x - 1], [y, y]] and [[x - 1, x], [y, y]] not in hh_bonds:
+                hh_bonds.append([[x, x - 1], [y, y]])
 
         if (
-            self.grid[f"{x, y - 1}"].filled
-            and (abs(self.grid[f"{x, y - 1}"].nodes[0].n - n) > 1)
-            and (self.grid[f"{x, y - 1}"].nodes[0].type == "H")
+            grid[f"{x, y - 1}"].filled
+            and (abs(grid[f"{x, y - 1}"].nodes[0].n - n) > 1)
+            and (grid[f"{x, y - 1}"].nodes[0].type == "H")
         ):
-            if [[x, x], [y, y - 1]] and [[x, x], [y - 1, y]] not in self.hh_bonds:
-                self.hh_bonds.append([[x, x], [y, y - 1]])
+            if [[x, x], [y, y - 1]] and [[x, x], [y - 1, y]] not in hh_bonds:
+                hh_bonds.append([[x, x], [y, y - 1]])
 
-    def update_neighbours(self):
-        self.hh_bonds = []
-        for key, value in self.grid_chain.items():
-            self.add_neighbours(self.grid[value[0]].nodes[0])
+    def update_neighbours(self, grid, grid_chain):
+        hh_bonds = []
+        for key, value in grid_chain.items():
+            self.add_neighbours(grid[value[0]].nodes[0], grid, hh_bonds)
 
-        self.stability = -len(self.hh_bonds)
+        return -len(hh_bonds), hh_bonds
 
-    def add_point(self, node, n):
+    def add_point(self, node, n, grid, grid_chain):
         x = node.x
         y = node.y
-        self.grid[f"{x, y}"].add_node(node)
-        self.grid_chain[n] = [f"{x, y}", [x, y]]
+        grid[f"{x, y}"].add_node(node)
+        grid_chain[n] = [f"{x, y}", [x, y]]
 
-    def clear_point(self, node, n):
+    def clear_point(self, node, n, grid):
         x = node.x
         y = node.y
-        self.grid[f"{x, y}"].remove_node(node)
+        grid[f"{x, y}"].remove_node(node)
 
-    def transfer_point(self, node1, x2, y2):
+    def transfer_point(self, node1, x2, y2, grid, grid_chain):
         n = node1.n
-        self.clear_point(node1, n)
+        self.clear_point(node1, n, grid)
         node1.x = x2
         node1.y = y2
-        self.add_point(node1, n)
+        self.add_point(node1, n, grid, grid_chain)
 
-    def overlap(self, x, y):
-        if self.grid[f"{x, y}"].filled:
+    def overlap(self, x, y, grid):
+        if grid[f"{x, y}"].filled:
             return True
 
         return False
 
-    def chain_stuck(self, x, y):
+    def chain_stuck(self, x, y, grid):
 
         if (
-            self.overlap(x + 1, y)
-            and self.overlap(x - 1, y)
-            and self.overlap(x, y + 1)
-            and self.overlap(x, y - 1)
+            self.overlap(x + 1, y, grid)
+            and self.overlap(x - 1, y, grid)
+            and self.overlap(x, y + 1, grid)
+            and self.overlap(x, y - 1, grid)
         ):
             return True
 
         return False
 
-    def check_diagonals(self, x, y):
+    def check_diagonals(self, x, y, grid):
         available_moves = []
 
-        if not self.grid[f"{x + 1, y + 1}"].filled:
+        if not grid[f"{x + 1, y + 1}"].filled:
             available_moves.append([1, 1])
 
-        if not self.grid[f"{x + 1, y - 1}"].filled:
+        if not grid[f"{x + 1, y - 1}"].filled:
             available_moves.append([1, -1])
 
-        if not self.grid[f"{x - 1, y + 1}"].filled:
+        if not grid[f"{x - 1, y + 1}"].filled:
             available_moves.append([-1, 1])
 
-        if not self.grid[f"{x - 1, y - 1}"].filled:
+        if not grid[f"{x - 1, y - 1}"].filled:
             available_moves.append([-1, -1])
 
         return available_moves
 
     def create_chain(self):
         index = 0
+        grid_chain = {}
 
         # Create grid
-        self.create_grid(36)
+        grid = self.create_grid(36)
         first_node = Node(0, 0)
         first_node.type = self.amino[0]
-        self.add_point(first_node, index)
+        self.add_point(first_node, index, grid, grid_chain)
 
         while index < len(self.amino) - 1:
-            current_node_key = self.grid_chain[index][0]
-            current_node = self.grid[current_node_key].nodes[0]
+            current_node_key = grid_chain[index][0]
+            current_node = grid[current_node_key].nodes[0]
 
             random_move = np.random.randint(len(self.moves))
 
             new_x = current_node.x + self.moves[random_move][0]
             new_y = current_node.y + self.moves[random_move][1]
 
-            if not self.overlap(new_x, new_y):
+            if not self.overlap(new_x, new_y, grid):
                 index += 1
                 new_node = Node(new_x, new_y)
                 new_node.n = index
                 new_node.type = self.amino[index]
-                self.add_point(new_node, index)
+                self.add_point(new_node, index, grid, grid_chain)
 
-            if self.chain_stuck(new_x, new_y):
+            if self.chain_stuck(new_x, new_y, grid):
                 index = 0
-                self.clear_grid()
-                self.grid_chain = {}
-                self.add_point(first_node, index)
+                self.clear_grid(grid)
+                grid_chain = {}
+                self.add_point(first_node, index, grid, grid_chain)
 
-    def create_vectors(self, node):
+        return grid_chain, grid
+
+    def create_vectors(self, node, grid_chain):
         node_i_coords = [node.x, node.y]
-        node_i1 = self.grid_chain[int(node.n) + 1]
+        node_i1 = grid_chain[int(node.n) + 1]
         node_i1_coords = np.array([node_i1[1][0], node_i1[1][1]])
         vector1 = node_i1_coords - np.array(node_i_coords)
 
         return node_i_coords, node_i1_coords, vector1
 
     def check_requirements(
-        self, available_moves, vector1, node_i_coords, node_i1_coords
+        self, available_moves, vector1, node_i_coords, node_i1_coords, grid
     ):
 
         for move in available_moves:
@@ -225,22 +224,22 @@ class Grid:
             C = L - vector1
 
             if (
-                (not self.overlap(L[0], L[1]))
-                and (not self.overlap(C[0], C[1]))
+                (not self.overlap(L[0], L[1], grid))
+                and (not self.overlap(C[0], C[1], grid))
                 and (np.linalg.norm(L - node_i1_coords) == 1.0)
             ):
                 return L, C, True
 
         return 0, 0, False
 
-    def pull_move(self, node):
+    def pull_move(self, node, grid, grid_chain):
 
-        node_i_coords, node_i1_coords, vector1 = self.create_vectors(node)
+        node_i_coords, node_i1_coords, vector1 = self.create_vectors(node, grid_chain)
 
-        available_moves = self.check_diagonals(node_i_coords[0], node_i_coords[1])
+        available_moves = self.check_diagonals(node_i_coords[0], node_i_coords[1], grid)
 
         L, C, check = self.check_requirements(
-            available_moves, vector1, node_i_coords, node_i1_coords
+            available_moves, vector1, node_i_coords, node_i1_coords, grid
         )
 
         if check:
@@ -248,88 +247,76 @@ class Grid:
             # residue of chain follows in footsteps
             for index in range(int(node.n - 1)):
 
-                residue_node_key = self.grid_chain[index][0]
-                residue_node = self.grid[residue_node_key].nodes[0]
+                residue_node_key = grid_chain[index][0]
+                residue_node = grid[residue_node_key].nodes[0]
 
-                residue_node_next_key = self.grid_chain[index + 2][0]
-                residue_node_next = self.grid[residue_node_next_key].nodes[0]
+                residue_node_next_key = grid_chain[index + 2][0]
+                residue_node_next = grid[residue_node_next_key].nodes[0]
 
                 self.transfer_point(
-                    residue_node, residue_node_next.x, residue_node_next.y
+                    residue_node,
+                    residue_node_next.x,
+                    residue_node_next.y,
+                    grid,
+                    grid_chain,
                 )
 
             # node moves to L
-            self.transfer_point(node, L[0], L[1])
+            self.transfer_point(node, L[0], L[1], grid, grid_chain)
 
             # Previous node moves to C
-            previous_node_key = self.grid_chain[int(node.n) - 1][0]
-            previous_node = self.grid[previous_node_key].nodes[0]
+            previous_node_key = grid_chain[int(node.n) - 1][0]
+            previous_node = grid[previous_node_key].nodes[0]
 
-            self.transfer_point(previous_node, C[0], C[1])
-        self.update_neighbours()
+            self.transfer_point(previous_node, C[0], C[1], grid, grid_chain)
+
+        return grid, grid_chain
 
     def hill_climber(self, max_iteration):
         # Current protein chain
-        self.create_chain()
-        self.current_hilltop = self.grid_chain
-        self.current_grid = self.grid
-
-        self.update_neighbours()
-        self.current_stability = self.stability
-        self.current_hh = self.hh_bonds
+        current_hilltop, grid = self.create_chain()
+        current_stability = self.update_neighbours(grid, current_hilltop)[0]
 
         # Save as best chain
-        self.best_c = self.current_hilltop
-        self.best_stab_c = self.current_stability
-        self.best_grid = self.current_grid
-        self.best_hh_bonds = self.current_hh
-        self.best_chain[self.best_stab_c] = [self.best_c, self.best_hh_bonds]
+        best_c = current_hilltop
+        best_stab_c = current_stability
+        best_grid = grid
+        self.best_chain[best_stab_c] = [best_c, best_grid]
 
         for iteration in range(max_iteration):
             best_c_found = False
             print(iteration)
 
             for it in range(100):
-                for index in range(1, len(self.current_hilltop) - 1):
+                for index in range(1, len(current_hilltop) - 1):
 
-                    self.pull_move(
-                        self.current_grid[self.current_hilltop[index][0]].nodes[0]
-                    )
-                    self.update_neighbours()
-                    temp_stability, temp_hh, temp_grid = (
-                        self.stability,
-                        self.hh_bonds,
-                        self.current_grid,
+                    temp_grid, temp_chain = self.pull_move(
+                        grid[current_hilltop[index][0]].nodes[0], grid, current_hilltop,
                     )
 
-                    if temp_stability < self.best_stab_c:
-                        self.best_c = self.grid_chain
-                        self.best_stab_c = temp_stability
-                        self.best_hh_bonds = temp_hh
-                        self.best_grid = temp_grid
+                    temp_stability = self.update_neighbours(temp_grid, temp_chain)[0]
+
+                    if temp_stability < best_stab_c:
+                        best_c = copy.deepcopy(temp_chain)
+                        best_grid = copy.deepcopy(temp_grid)
+                        best_stab_c = temp_stability
                         best_c_found = True
 
             if best_c_found:
-                self.current_hilltop = self.best_c
-                self.current_stability = self.best_stab_c
-                self.current_hh = self.best_hh_bonds
-                self.current_grid = self.best_grid
+                current_hilltop = copy.deepcopy(best_c)
+                current_stability = best_stab_c
+                grid = copy.deepcopy(best_grid)
                 best_c_found = False
 
             else:
-                self.best_chain[self.best_stab_c] = [self.best_c, self.best_hh_bonds]
-                self.create_chain()
-                self.current_hilltop = self.grid_chain
-                self.current_grid = self.grid
-                self.current_hh = self.hh_bonds
-                self.update_neighbours()
+                self.best_chain[best_stab_c] = [best_c, best_grid]
 
-                self.current_stability = self.stability
-                self.current_hh = self.hh_bonds
+                current_hilltop, grid = self.create_chain()
+                current_stability = self.update_neighbours(grid, current_hilltop)[0]
 
-                self.best_c = self.current_hilltop
-                self.best_stab_c = self.current_stability
-                self.hh_bonds = self.current_hh
+                best_c = current_hilltop
+                best_stab_c = current_stability
+                best_grid = grid
 
     def plot_chain(self):
         x = []
@@ -358,8 +345,10 @@ class Grid:
         best_chain_double = self.best_chain[best_chain_key]
 
         best_chain = best_chain_double[0]
-        best_hh_bonds = best_chain_double[1]
+        best_grid = best_chain_double[1]
 
+        best_stability, best_hh = self.update_neighbours(best_grid, best_chain)
+        print(best_stability)
         x = []
         y = []
 
@@ -367,14 +356,22 @@ class Grid:
             x.append(value[1][0])
             y.append(value[1][1])
 
-        for hh_bond in best_hh_bonds:
+        plt.plot(x, y, "ro-")
+
+        for key, value in best_chain.items():
+            node = best_grid[value[0]].nodes[0]
+            node_type = node.type
+
+            if node_type == "H":
+                plt.plot(node.x, node.y, "bo")
+
+        for hh_bond in best_hh:
             plt.plot(hh_bond[0], hh_bond[1], "y--")
 
-        plt.plot(x, y, "bo")
         plt.show()
 
 
 k = Grid("PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP")
-k.hill_climber(2)
+k.hill_climber(30)
 k.find_best_c()
 
